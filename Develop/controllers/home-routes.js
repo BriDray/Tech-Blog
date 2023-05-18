726a0fd84726e6bd5a64dc46bd6c64b4b3894b95
Bri Dray <stanggurl02@gmail.com>
2023-05-18 13:07:55 -0700

    added controllers

---
+++ Develop/controllers/home-routes.js
@@ -0,0 +1,66 @@
+// requires
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
+
+// getting posts for the homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// getting a SINGLE post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('single-post', { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Getting the login
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// router
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+// exporting
+module.exports = router;
