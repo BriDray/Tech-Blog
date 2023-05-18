418fbce61e15b9e9509c8302def6614409dc5156
Bri Dray <stanggurl02@gmail.com>
2023-05-18 12:45:19 -0700

    added a few more notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,19 @@
+// requiring all the things
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+// requiring express. adding 3001
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// requiring config and session
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// session
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -25,6 +29,7 @@ const sess = {
   })
 };
 
+// using session
 app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
@@ -32,12 +37,15 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// using express
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// connecting controller
 app.use(require('./controllers/'));
 
+// listening
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
